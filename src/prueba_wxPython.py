d3fae5bd3f62367376d2f70d37baa96693c59f0a
mateo.lourido.pena <mateo.lourido.pena@udc.es>
2025-10-13 19:30:21 +0200

    Inicio prueba

--- src/prueba_wxPython.py
+++ src/prueba_wxPython.py
@@ -1,1 +1,20 @@
 #Inicia
+
+import wx
+
+class TestFrame(wx.Frame):
+    def __init__(self, parent, ID, title):
+        wx.Frame.__init__(self, parent, wx.ID_ANY, title, pos=(0, 0), size=(320, 240))
+        panel = wx.Panel(self, wx.ID_ANY)
+        text = wx.StaticText(panel, wx.ID_ANY, "Hello, World!", wx.Point(10, 5), wx.Size(-1, -1))
+
+class TestApp(wx.App):
+    def OnInit(self):
+        frame = TestFrame(None, wx.ID_ANY, "Hello, world!")
+        self.SetTopWindow(frame)
+        frame.Show(True)
+        return True
+
+if __name__ == '__main__':
+    app = TestApp()
+    app.MainLoop()
