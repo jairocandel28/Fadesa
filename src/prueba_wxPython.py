8cca048f7ec4415e6d83ef94a90f3c3fd208c291
mateo.lourido.pena <mateo.lourido.pena@udc.es>
2025-10-15 13:52:43 +0200

    Añade más funciones a prueba_wxPython

--- src/prueba_wxPython.py
+++ src/prueba_wxPython.py
@@ -2,19 +2,68 @@
 
 import wx
 
-class TestFrame(wx.Frame):
-    def __init__(self, parent, ID, title):
-        wx.Frame.__init__(self, parent, wx.ID_ANY, title, pos=(0, 0), size=(320, 240))
-        panel = wx.Panel(self, wx.ID_ANY)
-        text = wx.StaticText(panel, wx.ID_ANY, "Hello, World!", wx.Point(10, 5), wx.Size(-1, -1))
+class Ventana(wx.Frame):
+    def __init__(self, *args, **kw):
+        super(Ventana, self).__init__(*args, **kw)
 
-class TestApp(wx.App):
-    def OnInit(self):
-        frame = TestFrame(None, wx.ID_ANY, "Hello, world!")
-        self.SetTopWindow(frame)
-        frame.Show(True)
-        return True
+        self.InitUI()
+
+    def InitUI(self):
+        panel = wx.Panel(self)
+
+        panel.SetBackgroundColour(wx.Colour(220, 220, 220))   # Color del fondo
+
+        # Para ponerle icono a la ventana, no funciona
+        """
+        bitmap = wx.Bitmap("mi_icono.png", wx.BITMAP_TYPE_PNG)
+        icono = wx.Icon()
+        icono.CopyFromBitmap(bitmap)
+        self.SetIcon(icono)
+        """
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        # Para poner imagen dentor de la ventana, no funciona
+        """
+        imagen = wx.Bitmap("Fadesa_logo.png", wx.BITMAP_TYPE_PNG)
+        img_widget = wx.StaticBitmap(panel, bitmap=imagen)
+        vbox.Add(img_widget, flag=wx.ALIGN_CENTER | wx.ALL, border=10)
+        """
+
+        self.text_ctrl = wx.TextCtrl(panel)
+        vbox.Add(self.text_ctrl, flag=wx.EXPAND | wx.ALL, border=10)
+
+        self.boton1 = wx.Button(panel, label='Mostrar lectura')
+        vbox.Add(self.boton1, flag=wx.ALIGN_CENTER | wx.ALL, border=10)
+        self.boton1.Bind(wx.EVT_BUTTON, self.OnBotonLectura)
+
+        self.boton2 = wx.Button(panel, label='Mostrar mensaje')
+        vbox.Add(self.boton2, flag=wx.ALIGN_CENTER | wx.ALL, border=10)
+        self.boton2.Bind(wx.EVT_BUTTON, self.OnBotonMensaje)
+
+        self.boton3 = wx.Button(panel, label='Cerrar programa')
+        vbox.Add(self.boton3, flag=wx.ALIGN_CENTER | wx.ALL, border=5)
+        self.boton3.Bind(wx.EVT_BUTTON, self.OnBotonCerrar)
+
+        panel.SetSizer(vbox)
+
+        self.SetTitle('Título')
+        self.SetSize((300, 200))
+        self.Centre()
+
+    def OnBotonLectura(self, event):
+        texto = self.text_ctrl.GetValue()
+        wx.MessageBox(f'Ingresaste: {texto}', 'Mensaje Introducido', wx.OK | wx.ICON_INFORMATION)
+
+    def OnBotonMensaje(self, event):
+        wx.MessageBox(f'Texto preestablecido por el creador.', 'Mensaje Fijo', wx.OK | wx.ICON_INFORMATION)
+
+    def OnBotonCerrar(self, event):
+        self.Close(True)
+
 
 if __name__ == '__main__':
-    app = TestApp()
+    app = wx.App()
+    ventana = Ventana(None)
+    ventana.Show()
     app.MainLoop()
