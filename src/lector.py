42a2e504ed665f825dbcf73deb3dd00a15cae27c
ander.asteinza <ander.asteinza@udc.es>
2025-10-20 18:44:54 +0200

    Corrige lector para que acepte formatos .db

--- src/lector.py
+++ src/lector.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlite3
 
 
 def leer_archivo(ruta):
@@ -7,9 +8,17 @@ def leer_archivo(ruta):
             datos = pd.read_csv(ruta)
         elif ruta.endswith((".xlsx", ".xls")):
             datos = pd.read_excel(ruta)
+        elif ruta.endswith(".db"):
+            conexion = sqlite3.connect(ruta)
+            cursor = conexion.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            tablas = [t[0] for t in cursor.fetchall()]
+            tabla = tablas[0]
+            datos = pd.read_sql_query(f"SELECT * FROM {tabla}", conexion)   
+            conexion.close()        
         else:
-            print("Formato no soportado. El archivo debe ser .csv o .xlsx/.xls.")
-            return
+            print("Formato no soportado. El archivo debe ser .csv, .xlsx/.xls. o .db")
+            return datos
 
         print("Datos cargados exitosamente:")
         print(datos.head(10))  # Muestra las primeras 10 filas del dataset
@@ -19,10 +28,15 @@ def leer_archivo(ruta):
         print(f"Error al leer el archivo: {e}")
 
 
+
+
 if __name__ == "__main__":
 
     
     archivo = input("Introduce la ruta completa del archivo que quieres analizar: ")
     resultado = leer_archivo(archivo)
+
+
+        
 
 
