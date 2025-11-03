03581dbb02bad77974d3cf16598ad9b751185a46
mateo.lourido.pena <mateo.lourido.pena@udc.es>
2025-11-03 13:31:27 +0100

    Añade documentación guardado y carga

--- src/modelo.py
+++ src/modelo.py
@@ -100,3 +100,34 @@ print(resultado_joblib.summary())
 
 # Joblib requiere de previa instlación a diferencia de Pickle que ya viene ingreado en la biblioteca por defecto de Python.
 # En cuanto a lo hora de utilizar las funciones de ambos métodos la sintaxis es similar siendo la de Joblib más simple a mi juicio.
+
+
+# Pasos para guardar y recuperar un modelo con Pickle:
+
+# Cuando ya tengamos un modelo entrenado para guardarlo en un archivo tenemos que utilizar with open() as file:; dentro del
+# paréntesis pondremos el nombre que le queramos dar al archivo entre comillas y una coma que lo separará de "wb" (write binary) que indicará 
+# su uso para el guardado. La extensión de dicho archivo debe ser .pickle. Debajo de esa línea y tabulado tendremos que poner el comando
+# pickle.dump(), y dentro del paréntesis el nombre de la variable que almacena el modelo y file. Un ejemplo funcional sería:
+# with open("modelo_pickle.pkl", "wb") as file:
+#    pickle.dump(modelo, file)
+
+# Para recuperar dicho modelo tendremos que escribir with open() as file:; nuevamente pero ahora dentro del paréntesis habrá que poner 
+# el nombre del archivo que queramos abrir entre comillas y una coma que lo separe de "rb" (read binary) que indica la letcura del archivo.
+# Debajo de esa línea y tabulado tendremos que poner una nueva varible que alamcenará los datos cargados con el comando pickle.load(), 
+# y dentro del paréntesis file. Un ejemplo funcional sería:
+# with open("modelo_pickle.pkl", "rb") as file:
+#    modelo_pickle = pickle.load(file)
+
+
+# Pasos para guardar y recuperar un modelo con Joblib:
+
+# Previamente necesitamos instalar la biblioteca (pip install joblib).
+
+# Cuando ya tengamos un modelo entrenado para guardarlo en un archivo tenemos que utilizar el comando joblib.dump() y dentro del
+# paréntesis pondremos el nombre de la variable en la que se encuentre el conjunto de datos del modelo y el nombre del archivo que 
+# queramos generar para almacenarla información. La extensión de dicho archivo puede ser .pickle por ejemplo, por lo que el guardado 
+# quedaría así: joblib.dump(modelo, "modelo_joblib.pkl").
+
+# Para recuperar dicho modelo tendremos que crear una nueva variable en la que guardaremos los datos que cargaremos y recibirá la 
+# información del archivo anteriormente creado, utilizaremos el comando joblib.load() y dentro del paréntesis se escribirá entre 
+# comillas el nombre del archivo que deseemos cargar, por ejemplo: modelo_joblib = joblib.load("modelo_joblib.pkl").
